905b3cb61e6d32417302e9c963562f6c9d4e7be8
404-error-404 <1791781644@qq.com>
2020-05-06 22:50:51 +0800

    更新了数据库，使用MySQL远程数据库

--- src/main/java/javaweb/remember/controller/HelloWorld.java
+++ src/main/java/javaweb/remember/controller/HelloWorld.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Time     : 2020-04-14 23:23:38
  * Author   : 张佳乐
@@ -20,7 +23,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorld {
 
     @GetMapping("/")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public String hello(
+            HttpServletRequest request,
+            @RequestParam(value = "name", defaultValue = "World") String name
+    ) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null){
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")){
+                    String token = cookie.getValue();
+                    System.out.println("准备进数据库");
+//                    User user = userMapper.findByToken(token); //去数据库寻找该token值的用户信息
+//                    System.out.println(user.toString());
+//                    if(user != null){ //若找到了这个用户信息
+//                        //写进session，让页面去展示
+//                        request.getSession().setAttribute("user",user);
+//                    }
+                    break;
+                }
+            }
+        }
+
+//        return "index";
+        
         return String.format("Hello %s!", name);
     }
 
